7c352607fc92dad4918ad865757cb4af464b926a
chandubhargav <110049878+chandubhargav921@users.noreply.github.com>
2023-08-11 03:05:27 +0530

    Add files via upload

---
+++ print_and_variables.py
@@ -0,0 +1,13 @@
+print("chandu")
+one =1;
+two=2;
+three=3;
+print(one ,two,three)
+print(one,two,three,sep="%")
+print(one,two,"hello world",end=" ")
+print(three)
+is_valid=True
+number =10
+pi =3.14
+name="kurnool"
+print(is_valid,number,pi,name,sep="^")
